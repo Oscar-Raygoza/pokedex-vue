726070169d4da809f9b28ccabc38ac0812917af1
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2022-02-18 16:31:10 -0600

    update dist

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/pokedex-vue/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/pokedex-vue/" : "/pokedex-vue/",
 };
